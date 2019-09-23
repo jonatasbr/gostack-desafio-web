5e34188f97ffcbd15505a4956085f74e9a886740
Jonatas Braz <jonatasbraz@gmail.com>
2019-09-23 02:30:43 -0300

    dev de autenticação

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,20 +1,25 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
 import * as Yup from 'yup';
 
+import { signInRequest } from '../../store/modules/auth/actions';
+
 import logo from '../../assets/logo.svg';
 
-export default function SignIn() {
-  const schema = Yup.object().shape({
-    email: Yup.string()
-      .email('Digite um e-mail válido')
-      .required('O e-mail é obrigatório'),
-    password: Yup.string().required('A senha é obrigatória'),
-  });
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email('Digite um e-mail válido')
+    .required('O e-mail é obrigatório'),
+  password: Yup.string().required('A senha é obrigatória'),
+});
 
-  function handleSubmit(data) {
-    console.tron.log(data);
+export default function SignIn() {
+  const dispatch = useDispatch();
+
+  function handleSubmit({ email, password }) {
+    dispatch(signInRequest(email, password));
   }
 
   return (
