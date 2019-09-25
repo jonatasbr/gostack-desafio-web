b2e94c5482dcb4ceaec2eeb0fa27e01e4e832cd5
Jonatas Braz <jonatasbraz@gmail.com>
2019-09-25 10:28:11 -0300

    estilização de Profile

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,7 +1,38 @@
 import React from 'react';
+import { Form, Input } from '@rocketseat/unform';
 
-// import { Container } from './styles';
+import { MdAddCircleOutline } from 'react-icons/md';
+
+import { Container } from './styles';
 
 export default function Profile() {
-  return <h1>Profile</h1>;
+  return (
+    <Container>
+      <Form>
+        <Input name="name" placeholder="Nome completo" />
+        <Input name="email" type="email" placeholder="Seu endereço de e-mail" />
+
+        <hr />
+
+        <Input
+          type="password"
+          name="oldPassword"
+          placeholder="Sua senha atual"
+        />
+        <Input type="password" name="password" placeholder="Nova senha" />
+        <Input
+          type="password"
+          name="confirmPassword"
+          placeholder="Confirmação de senha"
+        />
+
+        <div>
+          <button type="submit">
+            <MdAddCircleOutline color="#FFF" size={16} />
+            Salvar perfil
+          </button>
+        </div>
+      </Form>
+    </Container>
+  );
 }
